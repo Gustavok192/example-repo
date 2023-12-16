2ae2ba20158371c820baa090e630feca6227ba40
Gustavok192 <125589510+Gustavok192@users.noreply.github.com>
2023-12-16 14:16:56 -0500

    12/16/2013
    New update

---
+++ Ruby/learn-to-code-with-ruby/05 - Booleans and Predicate Methods/float_methods.rb
@@ -0,0 +1,24 @@
+puts 10.5.floor
+puts 10.9.floor
+puts 10.9.floor.class
+
+puts
+
+puts 10.5.ceil
+puts 10.8.ceil
+puts 10.2.ceil
+puts 10.2.ceil.class
+
+puts
+
+puts 3.14159.round
+puts 3.86.round
+puts 3.86.round.class
+puts 3.14159.round(2)
+puts 3.14859.round(2)
+puts 3.14859.round(3).class
+
+puts
+
+puts 5.35.abs
+puts -5.35.abs
