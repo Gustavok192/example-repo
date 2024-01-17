8a7312e7e7503de8bfbbaf1003ee4fcdf234a97c
Gustavok192 <125589510+Gustavok192@users.noreply.github.com>
2024-01-17 17:32:52 -0500

    1/17/2024

---
+++ Ruby/learn-to-code-with-ruby/07 - Blocks/the_step_method.rb
@@ -0,0 +1,7 @@
+#1.step(100, 5) { |number| puts "#{number}" }
+
+1.step(50, 7) do |number|
+  puts "Alright, let's go up by 7!"
+  puts "I'm now on #{number}"
+end
+
--- Ruby/learn-to-code-with-ruby/07 - Blocks/the_upto_and_downto_methods.rb
+++ Ruby/learn-to-code-with-ruby/07 - Blocks/the_upto_and_downto_methods.rb
@@ -1,3 +1,9 @@
-5.upto(10) {|current| puts "The loop is now on: #{current}" }
+# 5.upto(10) {|current| puts "The loop is now on: #{current}" }
 
-6.downto(1) { |number| puts "countdown: #{number}" }
+# 6.downto(1) { |number| puts "countdown: #{number}" }
+
+99.downto(1) do |number|
+  puts "#{number} bottles of beer on the wall, #{number} bottles of beer"
+  puts "Take one down, pass it around"
+  puts "#{number - 1} bottles of beer on the wall"
+end
