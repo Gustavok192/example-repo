0df9bf63ee40d7f964860df81628a930ad3f88a7
Gustavok192 <125589510+Gustavok192@users.noreply.github.com>
2024-02-21 10:17:39 -0500

    02/21/2024

--- Ruby/learn-to-code-with-ruby/08 - Conditionals/truthiness_and_falsiness.rb
+++ Ruby/learn-to-code-with-ruby/08 - Conditionals/truthiness_and_falsiness.rb
@@ -1,3 +1,17 @@
-if nil
-  puts "will this print"
+def even_or_odd_operations(num)
+  if num.even?  # Check if the number is even
+    return num + 2
+  else
+    return num - 3
+  end
 end
+
+def main
+  puts "Welcome to the Even or Odd Operations App!"
+  puts "Please enter an integer:"
+  input = gets.chomp.to_i  # Read input from the user and convert it to an integer
+  result = even_or_odd_operations(input)  # Call the method with the input
+  puts "Result: #{result}"
+end
+
+main
