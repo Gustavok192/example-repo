9aafe8cb54105ee4aa11d2830140b489d9272196
Gustavok192 <125589510+Gustavok192@users.noreply.github.com>
2024-04-24 22:10:29 -0500

    24/4/2024 strings

--- Ruby/learn-to-code-with-ruby/09 - Strings I/multiline_strings.rb
+++ Ruby/learn-to-code-with-ruby/09 - Strings I/multiline_strings.rb
@@ -1,1 +1,9 @@
-poetry = <<
+poetry = <<MLS
+  What a beutiful language Ruby is
+  It allows you to write multiple lines like this
+  Splits your words up across multiple lines
+  Everything still looks nice and fine.
+    - Shakespeare
+MLS
+
+puts poetry
---
+++ Ruby/learn-to-code-with-ruby/09 - Strings I/string_comparisons.rb
@@ -0,0 +1,25 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b
+puts a == c
+puts a == a 
+puts b == "hello"
+
+puts a != b 
+puts a != c
+
+puts 
+
+puts "A" < "B"
+puts "A" < "Z"
+puts "M" < "E" 
+
+puts "L" > "F"
+puts "L" > "D"
+
+puts "Z" < "a"
+puts "a" < "z"
+
+puts "hello" < "help"
