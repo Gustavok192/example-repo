08eeec9ea7e12ef817a7d876bc8a34a6d35aeba2
Gustavok192 <125589510+Gustavok192@users.noreply.github.com>
2024-04-24 21:36:50 -0500

    24/4/2024

---
+++ Ruby/learn-to-code-with-ruby/09 - Strings I/single_quotes_vs_double_quotes.rb
@@ -0,0 +1,10 @@
+puts "hello\n\n\nWorld"
+puts 'hello\n\n\nworld'
+
+# C:\Documents\news
+
+puts 'C:\Documents\news'
+
+phrase = "Hello world"
+puts "My phrase is #{phrase}"
+puts 'Myphrase is #{phrase}'
